70b4cb3d722b80a3cc1e8ade74ae1a391ed9c3ee
zdana <dana_zhao@foxmail.com>
2020-02-18 14:31:47 +0800

    学生信息

--- src/views/studentInfo.vue
+++ src/views/studentInfo.vue
@@ -1,7 +1,21 @@
 <template>
   <div class="studentInfo">
-    <h2>个人信息页面</h2>
-    <h2>当前状态: {{ student }}</h2>
+    <!-- <h2>个人信息页面</h2>
+    <h2>当前状态: {{ student }}</h2> -->
+
+    <v-card class="mx-auto mt-12 ml-6 mr-6">
+      <v-list-item three-line>
+        <v-list-item-content>
+          <div class="overline mb-4">
+            <span class="title font-weight-light">学生个人信息</span>
+          </div>
+          <v-list-item-title class="mb-1 mb-4">姓名：{{student.name}}</v-list-item-title>
+          <v-list-item-title class="mb-1 mb-4">性别：{{student.gender}}</v-list-item-title>
+          <v-list-item-title class="mb-1 mb-4">学号：{{student.number}}</v-list-item-title>
+        </v-list-item-content>
+        <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
+      </v-list-item>
+    </v-card>
   </div>
 </template>
 <script>
