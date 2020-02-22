56a9d1fe22fb33e51336ee504eec70dd85573be7
Shaojun Zhu <shj.zhu@gmail.com>
2020-02-22 08:33:54 +0800

    去掉history模式

--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,7 @@ import Login from "@/components/auth/Login.vue";
 Vue.use(VueRouter);
 
 const router = new VueRouter({
-  mode: "history",
+  // mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
