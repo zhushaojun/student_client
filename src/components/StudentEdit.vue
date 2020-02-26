b88df74bd4c9e6922c360bfc330a86fd5dd5092b
zdana <dana_zhao@foxmail.com>
2020-02-26 14:25:32 +0800

    修改

--- src/components/StudentEdit.vue
+++ src/components/StudentEdit.vue
@@ -4,16 +4,15 @@
       <v-list-item three-line>
         <v-list-item-content>
           <div class="overline mb-4">
-            <span class="title font-weight-light">学生信息</span>
+            <span class="title font-weight-light">学生个人信息</span>
           </div>
           <v-list-item-title class="mb-1 mb-4">姓名：{{ student.name }}</v-list-item-title>
           <v-list-item-title class="mb-1 mb-4">性别：{{ student.gender }}</v-list-item-title>
           <v-list-item-title class="mb-1 mb-4">学号：{{ student.number }}</v-list-item-title>
-          <v-text-field v-model="student.number" style="width:30%" dense></v-text-field>
         </v-list-item-content>
-        <!-- <v-list-item-avatar tile size="80" color="grey">
+        <v-list-item-avatar tile size="80" color="grey">
           <v-img class="elevation-6" :src="student.photo"></v-img>
-        </v-list-item-avatar>-->
+        </v-list-item-avatar>
       </v-list-item>
 
       <v-card class="mt-2 mb-2" min-height="380">
